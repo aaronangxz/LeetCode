80ef3c1ce738502061aba64ae01bb7c37d580ee3
aaronangxz <angxze@gmail.com>
2022-03-11 01:33:47 +0800

    uncomment struct

--- 21. Merge Two Sorted Lists.go
+++ 21. Merge Two Sorted Lists.go
@@ -1,10 +1,11 @@
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
+package mergeTwoLists
+
+//Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	if list1 == nil && list2 == nil {
 		return nil
